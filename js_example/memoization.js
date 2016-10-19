1245c74d6a0e03a310eff441b701d35e62ff4578
jinshengjiekk <jinshengjiekk@163.com>
2016-10-19 23:43:11 +0800

    modify memoization's comment

--- js_example/memoization.js
+++ js_example/memoization.js
@@ -4,6 +4,10 @@
  * ========================================================
  * Memoization ensures that a function doesn't run for the same inputs more than once
  * by keeping a record of the results for given inputs (usually in an object).
+ *
+ * Memoization is a common strategy for dynamic programming problems, which are problems where the solution is composed
+ * of solutions to the same problem with smaller inputs (as with the fibonacci problem, above). The other common strategy
+ * for dynamic programming problems is going bottom-up, which is usually cleaner and often more efficient.
  * ========================================================
  */
 
