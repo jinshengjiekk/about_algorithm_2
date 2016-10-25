620c4c77b880318fb5965c0761a13d976a8d1bbf
Hello King <jinshengjiekk@163.com>
2016-10-25 13:52:31 +0800

    modify  remove-duplicates-from-sorted-array.js

--- js_example/remove-duplicates-from-sorted-array.js
+++ js_example/remove-duplicates-from-sorted-array.js
@@ -14,20 +14,20 @@
 /**
  *
  * @param arr
- * @returns {number}
+ * @returns {number} 返回的仅仅是原数组被删除选定元素后的表象长度，数组的长度仍然不变，元素会有变动，后面的元素会重复。
  */
-function removeDuplicatesFromSortedArray(/*Array*/ arr) {
-	if(Object.prototype.toString.call(arr) !== '[object Array]'){
+function removeDuplicatesFromSortedArray_1(/*Array*/ arr) {
+	if (Object.prototype.toString.call(arr) !== '[object Array]') {
 		console.error('parameter error!');
 		throw new Error('the type of arguments must be Array!');
 	}
 
-	if(arguments.length > 1){
+	if (arguments.length > 1) {
 		console.warn("the arguments' length should be only one!");
 	}
 
 	let l = arr.length;
-	if(l){
+	if (!l) {
 		return 0;
 	}
 	let i = 0, j = 1;
@@ -40,11 +40,47 @@ function removeDuplicatesFromSortedArray(/*Array*/ arr) {
 	return j;
 }
 
+/**
+ * 此方法相对简单易懂
+ * @param arr
+ * @returns {number}
+ */
+function removeDuplicatesFromSortedArray_2(/*Array*/ arr) {
+	if (Object.prototype.toString.call(arr) !== '[object Array]') {
+		console.error('parameter error!');
+		throw new Error('the type of arguments must be Array!');
+	}
+
+	if (arguments.length > 1) {
+		console.warn("the arguments' length should be only one!");
+	}
+
+	let l = arr.length;
+	if (!l) {
+		return 0;
+	}
+	let i = 1, j = 0;
+	for (; i < l; i++) {
+		if (arr[i] !== arr[j]) {
+			arr[++j] = arr[i];
+		}
+	}
+	return j + 1;
+}
+
 
 //###################################################################
 const arr = [1, 1, 3, 3, 3, 4, 4, 5, 6, 7, 8,];
-console.group('removeDuplicatesFromSortedArray');
+console.group('removeDuplicatesFromSortedArray_1');
 console.info(arr);
-console.log(removeDuplicatesFromSortedArray(arr));
+console.log(removeDuplicatesFromSortedArray_1(arr));
 console.info(arr);
+console.groupEnd();
+
+//------------------------------------------------------------------
+const arr2 = [1, 1, 3, 3, 3, 4, 4, 5, 6, 7, 8,];
+console.group('removeDuplicatesFromSortedArray_2');
+console.info(arr2);
+console.log(removeDuplicatesFromSortedArray_2(arr2));
+console.info(arr2);
 console.groupEnd();
