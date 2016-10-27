a78542ebfc89b3b19544a0095b03297caf2af901
Hello King <jinshengjiekk@163.com>
2016-10-27 17:12:11 +0800

    modify  remove-duplicates-from-sorted-arrayII.js

--- js_example/remove-duplicates-from-sorted-arrayII.js
+++ js_example/remove-duplicates-from-sorted-arrayII.js
@@ -11,7 +11,11 @@
  */
 
 
-
+/**
+ *
+ * @param arr
+ * @returns {number}  返回的是过滤后的长度，数组长度仍然没有变，后面的元素存在重复现象
+ */
 function removeDuplicate_1(/*Array*/ arr) {
 	if (Object.prototype.toString.call(arr) !== '[object Array]') {
 		console.error('parameter error!');
@@ -22,4 +26,26 @@ function removeDuplicate_1(/*Array*/ arr) {
 		console.warn("the arguments' length should be only one!");
 	}
 
+	let i = 0, j = 0, l = arr.length;
+	for (; j < l; j++) {
+		if (i < 2 || arr[j] != arr[i-2]) {
+			arr[i++] = arr[j];
+		}
+	}
+	console.log(arr);
+	return i;
 }
+
+//##################################################################################
+const arr0 = [];
+const arr1 = [1, 1, 1, 2, 2, 2, 2, 3];
+const arr2 = [54, 32, 31, 31, 23, 23, 23, 23, 23, 23, 23, 21, 21, 10, 10, 10, 2, 1];
+const arr3 = [1, 2];
+const arr4 = [1];
+const arr5 = [1, 2, 3];
+console.info(removeDuplicate_1(arr0));
+console.info(removeDuplicate_1(arr1));
+console.info(removeDuplicate_1(arr2));
+console.info(removeDuplicate_1(arr3));
+console.info(removeDuplicate_1(arr4));
+console.info(removeDuplicate_1(arr5));
